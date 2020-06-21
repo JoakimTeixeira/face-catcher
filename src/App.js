f6dcc8a5253e528e96efcdb2a9491482cd5392b4
Joakim Teixeira <setokim1@hotmail.com>
2020-06-20 21:51:46 -0300

    Add particles animation

--- src/App.js
+++ src/App.js
@@ -4,10 +4,38 @@ import Navigation from "./components/Navigation/Navigation";
 import Logo from "./components/Logo/Logo";
 import ImageLinkForm from "./components/ImageLinkForm/ImageLinkForm";
 import Rank from "./components/Rank/Rank";
+import Particles from "react-particles-js";
+
+const particlesOptions = {
+  particles: {
+    number: {
+      value: 100,
+      density: {
+        enable: true,
+        value_area: 700,
+      },
+    },
+  },
+  interactivity: {
+    events: {
+      onhover: {
+        enable: true,
+        mode: "repulse",
+      },
+    },
+  },
+};
 
 function App() {
   return (
-    <div className="App">
+    <div className="app">
+      <Particles
+        style={{
+          width: "100%",
+        }}
+        className="particles"
+        params={particlesOptions}
+      />
       <Navigation />
       <Logo />
       <Rank />
