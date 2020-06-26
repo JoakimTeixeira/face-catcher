7be59cdecb18ddfb428d3044cc4b7ec4fc1cef25
Joakim Teixeira <setokim1@hotmail.com>
2020-06-25 23:41:26 -0300

    Add face recognition component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,16 @@
-import React from "react";
-import "./App.css";
+import React, { Component } from "react";
+import Particles from "react-particles-js";
+import Clarifai from "clarifai";
 import Navigation from "./components/Navigation/Navigation";
 import Logo from "./components/Logo/Logo";
 import ImageLinkForm from "./components/ImageLinkForm/ImageLinkForm";
 import Rank from "./components/Rank/Rank";
-import Particles from "react-particles-js";
+import FaceRecognition from "./components/FaceRecognition/FaceRecognition";
+import "./App.css";
+
+const app = new Clarifai.App({
+  apiKey: "test",
+});
 
 const particlesOptions = {
   particles: {
@@ -26,22 +32,51 @@ const particlesOptions = {
   },
 };
 
-function App() {
-  return (
-    <div className="app">
-      <Particles
-        style={{
-          width: "100%",
-        }}
-        className="particles"
-        params={particlesOptions}
-      />
-      <Navigation />
-      <Logo />
-      <Rank />
-      <ImageLinkForm />
-    </div>
-  );
+class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      input: "",
+      imageUrl: "",
+    };
+  }
+
+  onInputChange = event => {
+    this.setState({ input: event.target.value });
+  };
+
+  onButtonSubmit = () => {
+    this.setState({ imageUrl: this.state.input });
+    app.models
+      .predict(Clarifai.FACE_DETECT_MODEL, this.state.input)
+      .then(response => {
+        console.log(
+          response.outputs[0].data.regions[0].region_info.bounding_box
+        );
+      });
+  };
+
+  render() {
+    return (
+      <div className="app">
+        <Particles
+          style={{
+            width: "100%",
+          }}
+          className="particles"
+          params={particlesOptions}
+        />
+        <Navigation />
+        <Logo />
+        <Rank />
+        <ImageLinkForm
+          onInputChange={this.onInputChange}
+          onButtonSubmit={this.onButtonSubmit}
+        />
+        <FaceRecognition imageUrl={this.state.imageUrl} />
+      </div>
+    );
+  }
 }
 
 export default App;
--- src/components/ImageLinkForm/ImageLinkForm.js
+++ src/components/ImageLinkForm/ImageLinkForm.js
@@ -1,13 +1,15 @@
 import React from "react";
 import "./ImageLinkForm.css";
 
-const ImageLinkForm = () => {
+const ImageLinkForm = ({ onInputChange, onButtonSubmit }) => {
   return (
     <section className="detect">
       <p> This magic app will detect faces in your pictures. Give it a try. </p>
       <div className="detect-form">
-        <input className="detect-input" type="text" />
-        <button className="detect-btn">Detect</button>
+        <input className="detect-input" type="text" onChange={onInputChange} />
+        <button className="detect-btn" onClick={onButtonSubmit}>
+          Detect
+        </button>
       </div>
     </section>
   );
