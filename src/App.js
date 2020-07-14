e6c8a7ec243bdf01e107e2289ef8e8b3c5d8c1ab
Joakim Teixeira <setokim1@hotmail.com>
2020-07-14 11:00:45 -0300

    Implement API connection

--- src/App.js
+++ src/App.js
@@ -46,6 +46,13 @@ class App extends Component {
     };
   }
 
+  // Connect to API Rest using HTTP
+  componentDidMount() {
+    fetch("http://localhost:3000/")
+      .then(response => response.json())
+      .then(console.log);
+  }
+
   calculateFaceLocation = data => {
     const clarifaiFace =
       data.outputs[0].data.regions[0].region_info.bounding_box;
