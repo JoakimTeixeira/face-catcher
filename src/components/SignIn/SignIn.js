8b60f08382f7ed04fc1131c0ff04751ca581215b
Joakim Teixeira <setokim1@hotmail.com>
2020-07-14 12:58:42 -0300

    Implement API connection to signin route with state management

--- src/components/SignIn/SignIn.js
+++ src/components/SignIn/SignIn.js
@@ -1,51 +1,91 @@
-import React from "react";
+import React, { Component } from "react";
 import "./SignIn.css";
 
-const SignIn = ({ onRouteChange }) => {
-  return (
-    <article className="container">
-      <main className="signIn">
-        <div className="signIn-form">
-          <fieldset className="signIn-fieldset">
-            <legend className="signIn-legend">Sign In</legend>
+class SignIn extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      signInEmail: "",
+      signInPassword: "",
+    };
+  }
+
+  onEmailChange = event => {
+    this.setState({ signInEmail: event.target.value });
+  };
+
+  onPasswordChange = event => {
+    this.setState({ signInPassword: event.target.value });
+  };
+
+  // Connect to API Rest using HTTP
+  onSubmitSignIn = () => {
+    fetch("http://localhost:3000/signin", {
+      method: "post",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify({
+        email: this.state.signInEmail,
+        password: this.state.signInPassword,
+      }),
+    })
+      .then(response => response.json())
+      .then(data => {
+        if (data === "success") {
+          this.props.onRouteChange("home");
+        }
+      });
+  };
+
+  render() {
+    const { onRouteChange } = this.props;
+
+    return (
+      <article className="container">
+        <main className="signIn">
+          <div className="signIn-form">
+            <fieldset className="signIn-fieldset">
+              <legend className="signIn-legend">Sign In</legend>
+              <div className="signIn-wrapper">
+                <label className="signIn-label" htmlFor="email-address">
+                  Email
+                </label>
+                <input
+                  className="signIn-input"
+                  type="email"
+                  name="email-address"
+                  id="email-address"
+                  onChange={this.onEmailChange}
+                />
+              </div>
+              <div className="signIn-wrapper">
+                <label className="signIn-label" htmlFor="password">
+                  Password
+                </label>
+                <input
+                  className="signIn-input"
+                  type="password"
+                  name="password"
+                  id="password"
+                  onChange={this.onPasswordChange}
+                />
+              </div>
+            </fieldset>
             <div className="signIn-wrapper">
-              <label className="signIn-label" htmlFor="email-address">
-                Email
-              </label>
               <input
-                className="signIn-input"
-                type="email"
-                name="email-address"
-                id="email-address"
+                onClick={this.onSubmitSignIn}
+                className="signIn-input signIn-btn"
+                type="submit"
+                value="Sign in"
               />
+              <p onClick={() => onRouteChange("register")} className="register">
+                Register
+              </p>
             </div>
-            <div className="signIn-wrapper">
-              <label className="signIn-label" htmlFor="password">
-                Password
-              </label>
-              <input
-                className="signIn-input"
-                type="password"
-                name="password"
-                id="password"
-              />
-            </div>
-          </fieldset>
-          <div className="signIn-wrapper">
-            <input
-              onClick={() => onRouteChange("home")}
-              className="signIn-input signIn-btn"
-              type="submit"
-              value="Sign in"
-            />
-            <p onClick={() => onRouteChange("register")} className="register">
-              Register
-            </p>
           </div>
-        </div>
-      </main>
-    </article>
-  );
-};
+        </main>
+      </article>
+    );
+  }
+}
 
 export default SignIn;
