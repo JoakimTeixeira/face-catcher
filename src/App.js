da21f50653e191227715bf675fad866e48a3b214
Joakim Teixeira <setokim1@hotmail.com>
2020-07-20 09:28:20 -0300

    Change name of onButtonSubmit method

--- src/App.js
+++ src/App.js
@@ -88,13 +88,22 @@ class App extends Component {
     this.setState({ input: event.target.value });
   };
 
-  onButtonSubmit = () => {
+  onPictureSubmit = () => {
     this.setState({ imageUrl: this.state.input });
     app.models
       .predict(Clarifai.FACE_DETECT_MODEL, this.state.input)
-      .then(response =>
-        this.displayFaceBox(this.calculateFaceLocation(response))
-      )
+      .then(response => {
+        if (response) {
+          fetch("http://localhost:3000/image", {
+            method: "put",
+            headers: { "Content-Type": "application/json" },
+            body: JSON.stringify({
+              id: this.state.user.id,
+            }),
+          });
+        }
+        this.displayFaceBox(this.calculateFaceLocation(response));
+      })
       .catch(err => console.log(err));
   };
 
@@ -133,7 +142,7 @@ class App extends Component {
             />
             <ImageLinkForm
               onInputChange={this.onInputChange}
-              onButtonSubmit={this.onButtonSubmit}
+              onPictureSubmit={this.onPictureSubmit}
             />
             <FaceRecognition box={box} imageUrl={imageUrl} />
           </Fragment>
--- src/components/ImageLinkForm/ImageLinkForm.js
+++ src/components/ImageLinkForm/ImageLinkForm.js
@@ -1,13 +1,13 @@
 import React from "react";
 import "./ImageLinkForm.css";
 
-const ImageLinkForm = ({ onInputChange, onButtonSubmit }) => {
+const ImageLinkForm = ({ onInputChange, onPictureSubmit }) => {
   return (
     <section className="detect">
       <p> This magic app will detect faces in your pictures. Give it a try. </p>
       <div className="detect-form">
         <input className="detect-input" type="text" onChange={onInputChange} />
-        <button className="detect-btn" onClick={onButtonSubmit}>
+        <button className="detect-btn" onClick={onPictureSubmit}>
           Detect
         </button>
       </div>
